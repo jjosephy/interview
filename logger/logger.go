2366e633190c7e1fdc01a89e005f3cf446e33503
jjosephy <jjosephy@hotmail.com>
2016-12-12 14:40:18 -0800

    updated logger

--- logger/logger.go
+++ logger/logger.go
@@ -11,25 +11,27 @@ type Logger struct {
 	f *os.File
 }
 
+// LogInstance singleton instance in package
+var LogInstance *Logger
+
 // NewLogger creates a new instance of a logger
-func NewLogger() *Logger {
+func NewLogger() {
 	// open a file
 	f, err := os.OpenFile("logs/interview.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
 	if err != nil {
 		fmt.Printf("error opening file: %v", err)
 	}
+	log.SetOutput(f)
 
-	// don't forget to close it
-	defer f.Close()
-
-	//log.SetOutput(f)
-
-	log.Output(1, "this is an event")
-
-	return &Logger{}
+	LogInstance = &Logger{f: f}
 }
 
 // LogMsg logs a message
 func (l *Logger) LogMsg(msg string) {
+	log.Output(1, msg)
+}
 
+// Close closes the Logger
+func (l *Logger) Close() {
+	l.f.Close()
 }
